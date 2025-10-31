3f74119da60eb03154749eed8acd0a18d519146c
Emmanuel Salazar <60372654+EmmanuelSalazar@users.noreply.github.com>
2025-10-31 13:57:59 -0500

    .

--- src/components/EditOrderModal.jsx
+++ src/components/EditOrderModal.jsx
@@ -338,10 +338,24 @@ export const EditOrderModal = ({ isOpen, onClose, onSubmit, order, loading }) =>
               id="enProduccionEdit"
               name="enProduccion"
               checked={formData.enProduccion}
-              onChange={(e) => setFormData(prev => ({
-                ...prev,
-                enProduccion: e.target.checked
-              }))}
+              onChange={(e) => {
+                    const confirm = e.target.checked
+                    if (confirm) {
+                      const confirm = window.confirm('¿Estás seguro de marcar esta orden como en producción? Esto desactivará cualquier otra orden en producción del mismo modulo.')
+                      if (confirm) {
+                        console.log(e.target.checked)
+                         return setFormData(prev => ({
+                          ...prev,
+                          enProduccion: true  
+                        }))
+                      }
+                    } else {
+                        setFormData(prev => ({
+                          ...prev,
+                          enProduccion: false
+                        }))
+                      }
+                  }}
               className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
             />
             <label htmlFor="enProduccionEdit" className="text-sm font-medium text-blue-800">
